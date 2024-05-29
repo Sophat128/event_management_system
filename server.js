14b53e32799565cbcc951a8dd3752d49e1b2528f
Sophat <sophattann24@gmail.com>
2024-05-29 17:34:02 +0700

    event-management-system

---
+++ server.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+dotenv.config()
+
+const express = require("express");
+const userRoutes = require("./routers/userRoutes");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("common"))
+app.set("view engine", 'ejs')
+app.use(express.json())
+app.use("/assets", express.static("public"))
+app.use("",userRoutes)
+app.get("/", (req, res) => {
+  const style = "stylesheet"
+  res.send("<link rel='"+ style +"' href='/assets/css/styles.css'><h1 style='color: red'><img src='/assets/img/promise.webp'>NodeJS Project</h1>")
+})
+
+app.listen(process.env.PORT, ()=> {
+  console.log("Server is running on http://localhost");
+})
