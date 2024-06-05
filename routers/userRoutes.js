87aaeb26352720f3ae3d47c9c08387b5effca319
kimlyy <eangkimly2020@gmail.com>
2024-06-05 22:05:13 +0700

    user

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,11 +1,10 @@
 const userRoutes = require("express").Router();
 const userController = require("../controllers/userControllers")
 
-userRoutes.get("/users", userController.getAllUser)
-
-userRoutes.post("/users", userController.createNewUser)
-
-userRoutes.patch("/users/:id/updatePassword", userController.updatePassword)
-
+userRoutes.get("/api/users/:id", userController.getUserById)
+userRoutes.get("/api/allUsers", userController.getAllUsers)
+userRoutes.post("/api/users", userController.createNewUser)
+userRoutes.patch("/api/user/:id", userController.updateUser)
+userRoutes.delete("/api/user/:id", userController.deleteUser)
 
 module.exports = userRoutes;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const eventRoutes = require("./routers/eventRoutes");
 const morgan = require("morgan");
 const ticketRoutes = require("./routers/ticketRoutes");
 const app = express();
+const userRoutes = require("./routers/userRoutes");
 
 app.use(morgan("common"))
 app.set("view engine", 'ejs')
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/assets", express.static("public"))
 app.use("",eventRoutes)
 app.use("",ticketRoutes)
+app.use("",userRoutes)
 
 
 app.listen(process.env.PORT, ()=> {
